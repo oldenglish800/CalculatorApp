e27a65f5d22a8f5474af594ed3af2cea7a13c436
madri <madri@LAPTOP-B58DL28N>
2017-04-01 10:52:42 -0700

    Add project files.

---
+++ ConsoleApp13/CalculatorProject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CalculatorConsoleProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            decimal num01;
+            decimal num02;
+            int calcFunction;
+            decimal result;
+
+            Console.Write("Type your first number: ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What operation do you want to perform? ");
+            Console.WriteLine("1 for addition 2 for subtraction 3 for division 4 for multiplication");
+            calcFunction = Convert.ToInt32(Console.ReadLine());
+            switch (calcFunction)
+            {
+                case 1:
+                    Console.WriteLine("The result is " + (num01 + num02));
+                    break;
+                case 2:
+                    Console.WriteLine("The result is " + (num01 - num02));
+                    break;
+                case 3:
+                    result = (num01 / num02);
+                    Console.WriteLine("The result is " + (decimal)result);
+                    break;
+                case 4:
+                    Console.WriteLine("The result is " + (num01 * num02));
+                    break;
+
+                default:
+                    break;
+
+            } 
+            
+            Console.ReadKey();
+
+        }
+    }
+}
